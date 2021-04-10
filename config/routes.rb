62dba5eb5f03aff226ec6022b3e2b0ee3ce5746e
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-10 09:44:53 -0300

    Show created in controller and View

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # index route
   get "tasks", to: "tasks#index"
+
+  # display one task route
+  get "tasks/:id", to: "tasks#show", as: :task
+
 end
